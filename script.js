9bcc3aae216bc86d903f77eb868d87f08b52d2f9
Julio Chura <jchuraaca@unsa.edu.pe>
2024-09-07 23:54:20 -0500

    Se establecen propiedades generales2

---
+++ script.js
@@ -0,0 +1,5 @@
+const cloud = document.getElementById("cloud");
+const barralateral = document.querySelector(".barra-lateral")
+cloud.addEventListener("click", ()=> {
+    barralateral.classList.toggle("mini-barra-lateral")
+} )
